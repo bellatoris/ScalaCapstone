dc3ff562f7b765a58f9c381048f8d2be6f6164c8
bellatoris <bellatoris@snu.ac.kr>
2017-03-15 09:32:06 +0900

    [ScalaCapstone] Week1 compelte

--- observatory/src/main/scala/observatory/Extraction.scala
+++ observatory/src/main/scala/observatory/Extraction.scala
@@ -1,11 +1,49 @@
 package observatory
 
+import java.io.File
 import java.time.LocalDate
+
+import org.apache.spark.{RangePartitioner, SparkConf, SparkContext}
+import org.apache.spark.SparkContext._
+import org.apache.spark.rdd.RDD
 
 /**
   * 1st milestone: data extraction
   */
 object Extraction {
+
+  @transient lazy val conf: SparkConf = new SparkConf().setMaster("local[4]").setAppName("observatory")
+  @transient lazy val sc: SparkContext = new SparkContext(conf)
+
+  private def filePath(path: String) = {
+    new File(this.getClass.getClassLoader.getResource("." + path).toURI).getPath
+  }
+
+  def rawStation(lines: RDD[String]) =
+    lines.filter(line => {
+      val arr = line.split(",")
+      arr.size == 4 && arr(2) != "" && arr(3) != ""
+    }).map(line => {
+      val arr = line.split(",")
+      val stn = if (arr(0) == "") None else arr(0).toInt
+      val wban = if (arr(1) == "") None else arr(1).toInt
+      ((stn , wban) , Location(lat = arr(2).toDouble,
+                               lon = arr(3).toDouble))
+    })
+
+  def rawTemperature(lines: RDD[String], year: Int) =
+    lines.filter(line => {
+      val arr = line.split(",")
+      arr(2) != "" && arr(3) != "" && arr(4).toDouble != 9999.9
+    }).map(line => {
+      val arr = line.split(",")
+      val stn = if (arr(0) =="") None else arr(0).toInt
+      val wban = if (arr(1) == "") None else arr(1).toInt
+      val month = arr(2).toInt
+      val day = arr(3).toInt
+      val temperature = arr(4).toDouble
+      ((stn , wban) , (LocalDate.of(year, month, day), (temperature - 32) / 1.8))
+    })
 
   /**
     * @param year             Year number
@@ -14,7 +52,13 @@ object Extraction {
     * @return A sequence containing triplets (date, location, temperature)
     */
   def locateTemperatures(year: Int, stationsFile: String, temperaturesFile: String): Iterable[(LocalDate, Location, Double)] = {
-    ???
+    val temperature = sc.textFile(filePath(temperaturesFile))
+    val station = sc.textFile(filePath(stationsFile))
+    val rawS = rawStation(station)
+    val rawT = rawTemperature(temperature, year)
+
+    // need to ordering by date
+    rawT.join(rawS).map(x => (x._2._1._1, x._2._2, x._2._1._2)).collect()
   }
 
   /**
@@ -22,7 +66,7 @@ object Extraction {
     * @return A sequence containing, for each location, the average temperature over the year.
     */
   def locationYearlyAverageRecords(records: Iterable[(LocalDate, Location, Double)]): Iterable[(Location, Double)] = {
-    ???
+    records.groupBy(x => x._2).mapValues(x => x.aggregate(0.0)((x, y) => x + y._3, _ + _ ) / x.size).toList
   }
 
 }
--- observatory/src/main/scala/observatory/Main.scala
+++ observatory/src/main/scala/observatory/Main.scala
@@ -1,6 +1,7 @@
 package observatory
 
 object Main extends App {
-
-
+  val a = Extraction.locateTemperatures(2000, "/stations.csv", "/2000.csv")
+  val b = Extraction.locationYearlyAverageRecords(a)
+  println("hi")
 }
